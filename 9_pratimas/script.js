fd8a8c9967adce69c1c4329d49bed810a15a9c5c
blackoutcoder <romka19841201@gmail.com>
2022-02-24 18:22:52 +0200

    Task 9 completed.

--- 9_pratimas/script.js
+++ 9_pratimas/script.js
@@ -1,17 +1,24 @@
+/* ------------------------------ TASK 9 ---------------------------------------------------------------
+Sukurkite konstruktoriaus funkciją "Movie" (naudokte ES6), kuri sukuria objektus 3 savybėm ir 1 metodu:
+
+Savybės:
+title, director, budget
+Metodas: 
+wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kiru atveju false 
+------------------------------------------------------------------------------------------------------ */
+
 class Movie {
 	constructor(title, director, budget) {
 		this.title = title;
 		this.director = director;
 		this.budget = budget;
-
 		this.wasExpensive = () =>
-			console.log(`was expensive status:`, this.budget > 1e8);
+			console.log(`Budget is bigger than 100 000 000 mln USD - `, this.budget > 1e8);
 	}
 }
 
-const movie = new Movie("Avatar", "James Cameron", 237e6);
-
+const movie = new Movie("The Matrix - revolution", "Wacowsky sisters", 116e8);
 movie.wasExpensive();
-console.log(movie.title);
-console.log(movie.director);
-console.log(movie.budget);
+console.log("Movie title: " + movie.title);
+console.log("Directed by: " + movie.director);
+console.log("Budget: " + movie.budget + " USD.");
